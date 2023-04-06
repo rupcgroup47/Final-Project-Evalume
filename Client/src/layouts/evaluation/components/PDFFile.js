1c4cccb89124740db452a80f93511b29863a7ff8
NoaFarkash1 <ccgroup4711111@gmail.com>
2023-04-06 13:50:24 +0300

    pdf RTL

--- Client/src/layouts/evaluation/components/PDFFile.js
+++ Client/src/layouts/evaluation/components/PDFFile.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Page, Text, Document, StyleSheet } from "@react-pdf/renderer";
+import { Page, Text, Document, StyleSheet, Font } from "@react-pdf/renderer";
+import "@fontsource/rubik";
+Font.register({ family: 'Rubik', src: "http://fonts.gstatic.com/s/rubik/v3/4sMyW_teKWHB3K8Hm-Il6A.ttf" });// The only font works for Hebrew!
 
 const styles = StyleSheet.create({
   body: {
@@ -15,13 +17,15 @@ const styles = StyleSheet.create({
     margin: 12,
     fontSize: 14,
     textAlign: "justify",
-    fontFamily: "Times-Roman",
+    fontFamily: 'Rubik'
   },
   header: {
     fontSize: 12,
     marginBottom: 20,
     textAlign: "center",
     color: "grey",
+    fontFamily: 'Rubik'
+
   },
   pageNumber: {
     position: "absolute",
@@ -34,13 +38,14 @@ const styles = StyleSheet.create({
   },
 });
 
+
 const PDFFile = () => {
     return(
         <Document>
         <Page style={styles.body}>
-          <Text style={styles.header} fixed>jdtytu</Text>
-          <Text style={styles.text}>gdfhbhbcbc</Text>
-          {/* <Text style={styles.pageNumber} render={({pageNumber, totalPages}) => `${pageNumber} / ${totalPages}`} fixed></Text> */}
+          <Text style={styles.header} fixed>שלום</Text>
+          <Text style={styles.text} >מלל בעברית</Text>
+          <Text style={styles.pageNumber} render={({pageNumber, totalPages}) => `${pageNumber} / ${totalPages}`} fixed></Text>
         </Page>
       </Document>
     )
