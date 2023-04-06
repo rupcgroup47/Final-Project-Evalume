2afca899af3b1ea4134397cbe845d7df16f56a75
Shir <shirziv100@gmail.com>
2023-04-06 22:02:56 +0300

    add evalu_ques get process
    shir

---
+++ Final_Server/Final_Server/Controllers/EvaluationQuesController.cs
@@ -0,0 +1,63 @@
+﻿using Final_Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Text.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Final_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EvaluationQuesController : ControllerBase
+    {
+        // GET: api/<EvaluationQuesController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        [HttpGet]
+        public Object Get(bool quesType) //get all questions that fill the quesType
+        {
+            EvaluationQues e = new EvaluationQues();
+            return e.getFitQues(quesType);
+        }
+
+
+
+        // GET api/<EvaluationQuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EvaluationQuesController>
+        //[HttpPost]
+        //public int Post(JsonElement data)
+        //{
+        //    bool QuesType = Convert.ToBoolean(data.GetProperty("QuesType").GetInt32());
+        //    int RoleGroup_Type = Convert.ToInt32(data.GetProperty("RoleGroup_Type").GetInt32());
+        //    JsonElement group = data.GetProperty("Ques_Group");
+        //    string quesNum = group.GetProperty("QuesNum").ToString();
+
+
+        //}
+
+
+
+        // PUT api/<EvaluationQuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EvaluationQuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Final_Server/Final_Server/Models/EvaluationQues.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Final_Server.Models
+{
+    public class EvaluationQues
+    {
+        int questionnaireNum;
+        DateTime quesInsertDate = new DateTime();
+        bool quesType;
+        DateTime quesLimitDate = new DateTime();
+        int roleGroup_Type;
+
+        public int QuestionnaireNum { get => questionnaireNum; set => questionnaireNum = value; }
+        public DateTime QuesInsertDate { get => quesInsertDate; set => quesInsertDate = value; }
+        public bool QuesType { get => quesType; set => quesType = value; }
+        public int RoleGroup_Type { get => roleGroup_Type; set => roleGroup_Type = value; }
+        public DateTime QuesLimitDate { get => quesLimitDate; set => quesLimitDate = value; }
+
+        public Object getFitQues(bool quesType) //get all questions that fill the quesType
+        {
+            DBservices dbs = new DBservices();
+            return dbs.GetFitQues(quesType);
+        }
+    }
+}
+
+
