e522aa952dda8ee4cde4778a7273cb1300de8f60
Shir <shirziv100@gmail.com>
2023-04-12 19:00:53 +0300

    new
    shir

--- Final_Server/Final_Server/DAL/DBservices.cs
+++ Final_Server/Final_Server/DAL/DBservices.cs
@@ -1757,4 +1757,97 @@ public class DBservices
 
     //    return cmd;
     //}
+
+
+    ////--------------------------------------------------------------------------------------------------
+    //// This method gets all Questions
+    ////--------------------------------------------------------------------------------------------------
+    public List<object> GetGoals(int userNum)
+    {
+
+        SqlConnection con;
+        SqlCommand cmd;
+
+        try
+        {
+            con = connect("myProjDB"); // create the connection
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        cmd = CreateCommandWithSPGetGoalsByManager("spGetQuesByManagerNum", con, userNum);            // create the command
+
+        List<Object> List = new List<Object>();
+
+        try
+        {
+            SqlDataReader dataReader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            //dt.Load(dataReader);
+
+            while (dataReader.Read())
+            {
+
+                Object goal = new Object();
+
+                List.Add(new
+                {
+
+                    GoalNum = dataReader["GoalNum"],
+                    GoalName = dataReader["GoalName"],
+                    GoalGroup_Type = dataReader["GoalGroup_Type"],
+                    GoalGroup_Desc = dataReader["GoalGroup_Desc"],
+
+                    GoalCreateDate = dataReader["GoalCreateDate"],
+                    UserManagerNum = dataReader["UserManagerNum"],
+                    UserNum = dataReader["UserNum"],
+                    UserFName = dataReader["UserFName"],
+                    UserLName = dataReader["UserLName"],
+                    GoalStatus = dataReader["GoalStatus"]
+                }) ;
+            }
+
+            return List;
+        }
+        catch (Exception ex)
+        {
+            // write to log
+            throw (ex);
+        }
+
+        finally
+        {
+            if (con != null)
+            {
+                // close the db connection
+                con.Close();
+            }
+        }
+
+    }
+
+    //---------------------------------------------------------------------------------
+    // Create the SqlCommand using a stored procedure to get all Questions
+    //---------------------------------------------------------------------------------
+    private SqlCommand CreateCommandWithSPGetGoalsByManager(String spName, SqlConnection con, int userNum)
+    {
+
+        SqlCommand cmd = new SqlCommand(); // create the command object
+
+        cmd.Connection = con;              // assign the connection to the command object
+
+        cmd.CommandText = spName;      // can be Select, Insert, Update, Delete 
+
+        cmd.CommandTimeout = 10;           // Time to wait for the execution' The default is 30 seconds
+
+        cmd.CommandType = System.Data.CommandType.StoredProcedure; // the type of the command, can also be stored procedure
+
+        cmd.Parameters.AddWithValue("@UserNum", userNum); //insert all the parameters we got from the user
+
+
+        return cmd;
+    }
+
 }
