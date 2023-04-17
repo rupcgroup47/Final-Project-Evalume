22f767ab55afa7f87540d05fecae30b45dcc9a87
NoaFarkash1 <ccgroup4711111@gmail.com>
2023-04-17 23:05:26 +0300

    k

--- Client/src/context/evalueVariables.jsx
+++ Client/src/context/evalueVariables.jsx
@@ -34,41 +34,7 @@ export default function EvalueContextProvider({ children }) {
     // apiGetEmployeeStatus: "https://proj.ruppin.ac.il/cgroup47/prod/userNum?userNum=", //server
   });
 
-  //   const InitNotesContext = [ 
-  //     {
-  //         "id": 1,
-  //         "title": "Note 1",
-  //         "description":"Do presentation",
-  //         "category":"Work"
-  //       },
-  //       {
-  //         "id": 2,
-  //         "title": "Note 2",
-  //         "description":"Call Noa",
-  //         "category":"Work"
-
-  //       }, 
-  //      {
-  //         "id": 3,
-  //         "title": "Note 3",
-  //         "description":"Call Yarden",
-  //         "category":"Work"
-
-  //       },
-  //       {
-  //         "id": 4,
-  //         "title": "Note 4",
-  //         "description":"Send Email",
-  //         "category":"Personal"
-
-  //       }
-  // ]
-
-
-  //   const [notesArrContext, SetNotesArrContext] = useState(InitNotesContext);
-  //   const InitCategoryArrContext = [ "Work", "Personal", "Ideas","Lists"]
-  //   const [CategoryArrContext, SetCategoryArrContext] = useState(InitCategoryArrContext)
-  //   const [currentCat, setCurrentCat] = useState("")
+  
   const [chosenEmployee, setChosenEmployee] = useState("") //need to set to loacl storage beacuse it is not saved when refreshing
 
   useEffect(() => {
