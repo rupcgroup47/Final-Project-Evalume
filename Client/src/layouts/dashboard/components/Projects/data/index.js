244329e6098375288f77afaf388ee496117eac80
NoaFarkash1 <ccgroup4711111@gmail.com>
2023-05-30 22:23:16 +0300

    bi table

--- Client/src/layouts/dashboard/components/Projects/data/index.js
+++ Client/src/layouts/dashboard/components/Projects/data/index.js
@@ -2,84 +2,54 @@
 // @mui material components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-import MDAvatar from "components/MDAvatar";
 import MDProgress from "components/MDProgress";
+
+const array = [ { name: "משאבי אנוש", depCount: 100, level0:30, level1:40, level2:30, progress: 60 },
+{ name: "לוגיסטיקה", depCount: 300, level0:200, level1:40, level2:60, progress: 10 }]
 
 
 export default function data() {
-  const Company = ({ image, name }) => (
-    <MDBox display="flex" alignItems="center" lineHeight={1}>
-      <MDAvatar src={image} name={name} size="sm" />
-      <MDTypography variant="button" fontWeight="medium" ml={1} lineHeight={1}>
-        {name}
-      </MDTypography>
-    </MDBox>
-  );
+
 
   return {
     columns: [
-      { Header: "מחלקות", accessor: "companies", width: "45%", align: "left" },
-      // { Header: "סיימו לענות", accessor: "members", width: "10%", align: "left" },
+      { Header: "מחלקות", accessor: "departments", width: "45%", align: "left" },
       { Header: "כמות עובדים במחלקה", accessor: "depCount", align: "center" },
-      { Header: "הערכה עצמית", accessor: "level1", align: "center" },
-      { Header: "הערכת מנהל", accessor: "level2", align: "center" },
-      { Header: "הערכה משותפת", accessor: "level3", align: "center" },
+      { Header: "הערכה עצמית", accessor: "level0", align: "center" },
+      { Header: "הערכת מנהל", accessor: "level1", align: "center" },
+      { Header: "הערכה משותפת", accessor: "level2", align: "center" },
       { Header: " התקדמות", accessor: "completion", align: "center" },
     ],
 
-    rows: [
-      {
-        companies: <Company name="משאבי אנוש" />,
-        depCount: (
-          <MDTypography variant="caption" color="text" fontWeight="medium">
-            100
-          </MDTypography>
-        ),
-        completion: (
-          <MDBox width="8rem" textAlign="left">
-            <MDProgress value={60} color="info" variant="gradient" label={false} />
-          </MDBox>
-        ),
-      },
-      {
-        companies: <Company name="לוגיסטיקה" />,
-        depCount: (
-          <MDTypography variant="caption" color="text" fontWeight="medium">
-            300
-          </MDTypography>
-        ),
-        completion: (
-          <MDBox width="8rem" textAlign="left">
-            <MDProgress value={10} color="info" variant="gradient" label={false} />
-          </MDBox>
-        ),
-      },
-      {
-        companies: <Company name="כספים" />,
-        depCount: (
-          <MDTypography variant="caption" color="text" fontWeight="medium">
-            100
-          </MDTypography>
-        ),
-        completion: (
-          <MDBox width="8rem" textAlign="left">
-            <MDProgress value={100} color="success" variant="gradient" label={false} />
-          </MDBox>
-        ),
-      },
-      {
-        companies: <Company  name="מערכות מידע" />,
-        depCount: (
-          <MDTypography variant="caption" color="text" fontWeight="medium">
-           35
-          </MDTypography>
-        ),
-        completion: (
-          <MDBox width="8rem" textAlign="left">
-            <MDProgress value={100} color="success" variant="gradient" label={false} />
-          </MDBox>
-        ),
-      }
-    ],
+    rows: array.map((item) => ({
+      departments:  <MDTypography variant="caption" color="text" fontWeight="medium">
+      {item.name}
+    </MDTypography>,
+      depCount: (
+        <MDTypography variant="caption" color="text" fontWeight="medium">
+          {item.depCount}
+        </MDTypography>
+      ),
+      level0: (
+        <MDTypography variant="caption" color="text" fontWeight="medium">
+          {item.level0}
+        </MDTypography>
+      ),
+      level1: (
+        <MDTypography variant="caption" color="text" fontWeight="medium">
+          {item.level1}
+        </MDTypography>
+      ),
+       level2: (
+        <MDTypography variant="caption" color="text" fontWeight="medium">
+          {item.level2}
+        </MDTypography>
+      ),
+      completion: (
+        <MDBox width="8rem" textAlign="left">
+          <MDProgress value={item.progress} color="info" variant="gradient" label={false} />
+        </MDBox>
+      ),
+    })),
   };
 }
--- Client/src/layouts/dashboard/components/Projects/index.js
+++ Client/src/layouts/dashboard/components/Projects/index.js
@@ -30,43 +30,44 @@ import DataTable from "examples/Tables/DataTable";
 
 // Data
 import data from "layouts/dashboard/components/Projects/data";
+import { Today } from "@mui/icons-material";
 
 function Projects() {
   const { columns, rows } = data();
   const [menu, setMenu] = useState(null);
-
+const year= new Date().getFullYear();
   const openMenu = ({ currentTarget }) => setMenu(currentTarget);
   const closeMenu = () => setMenu(null);
 
-  const renderMenu = (
-    <Menu
-      id="simple-menu"
-      anchorEl={menu}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "left",
-      }}
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      open={Boolean(menu)}
-      onClose={closeMenu}
-    >
-      <MenuItem onClick={closeMenu}>Action</MenuItem>
-      <MenuItem onClick={closeMenu}>Another action</MenuItem>
-      <MenuItem onClick={closeMenu}>Something else</MenuItem>
-    </Menu>
-  );
+  // const renderMenu = (
+  //   <Menu
+  //     id="simple-menu"
+  //     anchorEl={menu}
+  //     anchorOrigin={{
+  //       vertical: "top",
+  //       horizontal: "left",
+  //     }}
+  //     transformOrigin={{
+  //       vertical: "top",
+  //       horizontal: "right",
+  //     }}
+  //     open={Boolean(menu)}
+  //     onClose={closeMenu}
+  //   >
+  //     <MenuItem onClick={closeMenu}>Action</MenuItem>
+  //     <MenuItem onClick={closeMenu}>Another action</MenuItem>
+  //     <MenuItem onClick={closeMenu}>Something else</MenuItem>
+  //   </Menu>
+  // );
 
   return (
     <Card>
       <MDBox display="flex" justifyContent="space-between" alignItems="center" p={3}>
         <MDBox>
           <MDTypography variant="h6" gutterBottom>
-            קצב מילוי השאלון לפי מחלקות לשנת 2023
+            קצב מילוי השאלון לפי מחלקות לשנת {year}
           </MDTypography>
-          <MDBox display="flex" alignItems="center" lineHeight={0}>
+          {/* <MDBox display="flex" alignItems="center" lineHeight={0}>
             <Icon
               sx={{
                 fontWeight: "bold",
@@ -79,14 +80,14 @@ function Projects() {
             <MDTypography variant="button" fontWeight="regular" color="text">
               &nbsp;<strong>150 ענו</strong> בשבוע האחרון
             </MDTypography>
-          </MDBox>
+          </MDBox> */}
         </MDBox>
-        <MDBox color="text" px={2}>
+        {/* <MDBox color="text" px={2}>
           <Icon sx={{ cursor: "pointer", fontWeight: "bold" }} fontSize="small" onClick={openMenu}>
             more_vert
           </Icon>
-        </MDBox>
-        {renderMenu}
+        </MDBox> */}
+        {/* {renderMenu} */}
       </MDBox>
       <MDBox>
         <DataTable
